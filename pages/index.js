56d5fe6eb4e167ad963a06f0a178afc37dc5df32
Daniel Padula <hola@danielpadula.com>
2021-07-25 23:47:04 -0300

    Link

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -7,6 +8,37 @@ export default function Home() {
       <Head>
         <title>Daniel Padula</title>
       </Head>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link href="/">
+            <a className="navbar-brand" href="#">
+              Daniel Padula
+            </a>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link href="/resume">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Resume
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
